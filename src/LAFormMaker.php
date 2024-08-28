fc63edbbef94e2eddcea54b9feda91c8a026271d
Wagner Oliveira <64755674+Wagner-23@users.noreply.github.com>
2024-08-28 18:06:45 -0300

    ajuste para criar opção vazia no select

--- src/LAFormMaker.php
+++ src/LAFormMaker.php
@@ -201,6 +201,8 @@ class LAFormMaker
 					} else {
 						$popup_vals = array();
 					}
+					// Adicionando o item vazio como primeira opção
+                    $popup_vals = array_merge(['' => 'Selecione uma opção'], $popup_vals);
 					$out .= Form::select($field_name, $popup_vals, $default_val, $params);
 					//ver no vendor\laravelcollective\html\src\formbuilder.php   > função select
 					break;
